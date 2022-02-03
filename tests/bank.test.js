3b82d3dd192985ec7a324641a7fa95a030f27003
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-03 11:56:27 +0000

    extra test added for debit being the most recent withdraw amount

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -21,6 +21,11 @@ describe('Bank class', () => {
       bank.withdraw(100);
       expect(bank.debit).toBe(100);
     });
+    it('only changes debit by the most recent withdraw amount', () => {
+      bank.withdraw(100);
+      bank.withdraw(50);
+      expect(bank.debit).toBe(50);
+    });
   })
  
 });
