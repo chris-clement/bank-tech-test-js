e1de61dcaef294c7cc1efb15d967e5df1b3ae473
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-03 11:47:17 +0000

    Removed unnecessary test which were passing regardless of whether the method was there or not

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -6,18 +6,18 @@ describe('Bank class', () => {
   });
 
   describe('#deposit', () => {
-    it('is a method within Bank', () => {
-      expect(bank.deposit).toBeTruthy;
-    });
     it('changes credit by the amount passed as an argument', () => {
-      bank.deposit(100)
+      bank.deposit(100);
       expect(bank.credit).toBe(100);
     });
     it('only changes credit by the most recent deposit amount', () => {
-      bank.deposit(100)
-      bank.deposit(50)
+      bank.deposit(100);
+      bank.deposit(50);
       expect(bank.credit).toBe(50);
     });
   });
+  describe('#withdraw', () => {
+  
+  })
  
 });
