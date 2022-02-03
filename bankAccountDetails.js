f62dae819ad7d1d5910f74dead943023f7b74e64
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-03 15:28:21 +0000

    sort_history_old_to_new holds logic that was being done in addBalanceToHistory

--- bankAccountDetails.js
+++ bankAccountDetails.js
@@ -1,15 +1,19 @@
 class bankAccountDetails {
   constructor() {
   };
+  sort_history_old_to_new(history) {
+    history.sort((transaction_a, transaction_b) => transaction_a.date >= transaction_b.date? 1 : -1);
+  };
   addBalanceToHistory(history) {
     this.balance_amount = 0
-    history.sort((transaction_a, transaction_b) => transaction_a.date >= transaction_b.date? 1 : -1);
+    this.sort_history_old_to_new(history)
     history.forEach(transaction => {
       this.balance_amount += transaction['credit'] - transaction['debit']
       transaction["balance"] = this.balance_amount
     });
     return history;
   };
+ 
 };
 
 module.exports = bankAccountDetails;
