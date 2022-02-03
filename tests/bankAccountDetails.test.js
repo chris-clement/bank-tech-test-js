6ea243abca951b616070eb63e59a1eb0ef753f7c
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-03 14:43:33 +0000

    addBalanceToHistory works for a single debit or credit transaction

--- tests/bankAccountDetails.test.js
+++ tests/bankAccountDetails.test.js
@@ -5,8 +5,11 @@ describe('bankAccountDetails class', () => {
     bankDetails = new bankAccountDetails();
   });
   describe('#addBalanceToHistory', () => {
-    it('calculates the correct balance for a history with a single transaction', () => {
+    it('calculates the correct balance for a history with a single debit', () => {
       expect(bankDetails.addBalanceToHistory([{date: new Date(2022, 1, 1), debit: 100, credit: 0}])).toEqual([{date: new Date(2022, 1, 1), debit: 100, credit: 0, balance: -100}]);
+    });
+    it('calculates the correct balance for a history with a single credit', () => {
+      expect(bankDetails.addBalanceToHistory([{date: new Date(2022, 1, 1), debit: 0, credit: 100}])).toEqual([{date: new Date(2022, 1, 1), debit: 0, credit: 100, balance: 100}]);
     });
   });
 });
